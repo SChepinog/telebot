abc33dda8382c1901fb7d3e2b38d9b9128da4d3b
SChepinog <chepinoga-sv@yandex.ru>
2020-11-12 16:19:57 +0300

    private methods

--- game_container.py
+++ game_container.py
@@ -10,7 +10,7 @@ class Game:
         self.is_started = False
 
     def start(self):
-        self.secret = generate_secret(self.secret_length)
+        self.secret = self.__generate_secret(self.secret_length)
         self.try_count = 0
         self.is_started = True
         return self
@@ -28,11 +28,11 @@ class Game:
                 self.stop()
                 return 'Victory! It toke ' + str(self.try_count) + ' tries'
             else:
-                result = self.check_string(try_secret)
+                result = self.__check_string(try_secret)
                 self.try_count += 1
                 return "Try again. It's your " + str(self.try_count) + " try\nResult: " + result
 
-    def check_string(self, try_secret):
+    def __check_string(self, try_secret):
         bulls = 0
         cows = 0
         for i in range(0, len(self.secret)):
@@ -42,14 +42,14 @@ class Game:
                 cows += 1
         return 'Bulls: ' + str(bulls) + ', Cows: ' + str(cows)
 
-
-def generate_secret(param):
-    numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
-    if int(param) > 10:
-        return Exception("Bad number")
-    result = ''
-    for i in range(0, int(param)):
-        selected = random.choice(numbers)
-        numbers.remove(selected)
-        result += selected
-    return result
+    @staticmethod
+    def __generate_secret(param):
+        numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+        if int(param) > 10:
+            return Exception("Bad number")
+        result = ''
+        for i in range(0, int(param)):
+            selected = random.choice(numbers)
+            numbers.remove(selected)
+            result += selected
+        return result
