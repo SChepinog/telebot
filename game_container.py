bc41e38531a77dc45096fde2fafce79ef28a0ec0
SChepinog <chepinoga-sv@yandex.ru>
2020-11-12 16:06:24 +0300

    Game is working

--- game_container.py
+++ game_container.py
@@ -10,7 +10,7 @@ class Game:
         self.is_started = False
 
     def start(self):
-        self.secret = self.generate_secret(self.secret_length)
+        self.secret = generate_secret(self.secret_length)
         self.try_count = 0
         self.is_started = True
         return self
@@ -26,19 +26,30 @@ class Game:
             elif try_secret == self.secret:
                 self.try_count += 1
                 self.stop()
-                return 'Victory! It toke ' + str(self.try_count)
+                return 'Victory! It toke ' + str(self.try_count) + ' tries'
             else:
+                result = self.check_string(try_secret)
                 self.try_count += 1
-                return "Try again. It's your " + str(self.try_count) + " try"
+                return "Try again. It's your " + str(self.try_count) + " try\nResult: " + result
 
-    @staticmethod
-    def generate_secret(param):
-        if int(param) > 10:
-            return Exception("Bad number")
-        numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
-        result = ''
-        for i in range(0, int(param)):
-            selected = random.choice(numbers)
-            numbers.remove(selected)
-            result += selected
-        return result
+    def check_string(self, try_secret):
+        bulls = 0
+        cows = 0
+        for i in range(0, len(self.secret)):
+            if self.secret[i] == try_secret[i]:
+                bulls += 1
+            if try_secret.__contains__(self.secret[i]):
+                cows += 1
+        return 'Bulls: ' + str(bulls) + ', Cows: ' + str(cows)
+
+
+def generate_secret(param):
+    numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+    if int(param) > 10:
+        return Exception("Bad number")
+    result = ''
+    for i in range(0, int(param)):
+        selected = random.choice(numbers)
+        numbers.remove(selected)
+        result += selected
+    return result
