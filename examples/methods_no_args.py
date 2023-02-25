2812917872a16e53bdbf425776ea6b803ee1637e
SChepinog <chepinoga-sv@yandex.ru>
2023-02-25 13:16:47 +0300

    Method with no args

---
+++ examples/methods_no_args.py
@@ -0,0 +1,12 @@
+def perimeter():
+    return (a + b) * 2
+
+
+def square():
+    return a * b
+
+
+a = int(input("Input length\n"))
+b = int(input("Input width\n"))
+print(perimeter())
+print(square())
